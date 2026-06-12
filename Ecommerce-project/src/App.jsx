04951d656889d2b1444ad274ffe7c1d22bc17aec
westbrown687 <ojiakofoe@gmail.com>
2026-06-12 11:42:09 +0100

    generate-the-html-for-the-deliveryOptions

--- Ecommerce-project/src/App.jsx
+++ Ecommerce-project/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   useEffect(() => {
     axios.get("/api/cart-items?expand=product").then((response) => {
       setCart(response.data);
-      //console.log(response.data);
+      // console.log(response.data);
     });
   }, []);
 
